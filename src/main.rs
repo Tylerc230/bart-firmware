1585a049ab84372a66f6a696cca52eebc54aaf56
Tyler Casselman <tylerc230@gmail.com>
2024-05-02 20:52:08 -0700

    Leds lighting up
    Just had to add a loop at the end so the spi bus didn't go out of scop

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use esp_idf_svc::{
 use ws2812_spi::Ws2812;
 
 use serde::Deserialize;
+use std::{thread, time};
+use smart_leds::RGB8;
+use smart_leds::SmartLedsWrite;
 
 #[toml_cfg::toml_config]
 pub struct Config {
@@ -47,25 +50,42 @@ fn main() -> Result<()>{
     //print!("{:?}", json);
     let pins = peripherals.pins;
     let spi_pin = peripherals.spi2;
-    let result = create_spi_bus(pins, spi_pin);
-    let spi_bus = match result {
-        Ok(s) => s,
-        Err(e) => {
-            log::info!("SPI error: {:?}", e);
-            return Err(e.into());
-        }
-    };
+    let spi_bus = create_spi_bus(pins, spi_pin)?;
     let mut leds = Ws2812::new(spi_bus); 
 
-    Ok(())
+    let delay = time::Duration::from_secs(3);
+    let mut data: [RGB8; 3] = [RGB8::default(); 3];
+    let empty: [RGB8; 3] = [RGB8::default(); 3];
+    loop {
+        data[0] = RGB8 {
+            r: 0,
+            g: 0,
+            b: 0x10,
+        };
+        data[1] = RGB8 {
+            r: 0,
+            g: 0x10,
+            b: 0,
+        };
+        data[2] = RGB8 {
+            r: 0x10,
+            g: 0,
+            b: 0,
+        };
+        leds.write(data.iter().cloned()).unwrap();
+        thread::sleep(delay);
+        leds.write(empty.iter().cloned()).unwrap();
+        thread::sleep(delay);
+    }
 }
 
 fn create_spi_bus<'a>(pins: gpio::Pins, spi_pin: spi::SPI2) -> Result<spi::SpiBusDriver<'a, SpiDriver<'a>>, EspError> {
-    let sclk = pins.gpio48;
-    let sdo = pins.gpio38;
+    let sclk = pins.gpio12;
+    let sdo = pins.gpio11;
+    let sdi = pins.gpio13;
     let spi_config = spi::SpiConfig::new().baudrate(3.MHz().into());
-    let mut spi_driver = SpiDriver::new::<SPI2>(spi_pin, sclk, sdo, Option::<gpio::Gpio21>::None, &SpiDriverConfig::new())?;
-    spi::SpiBusDriver::new(spi_driver, &spi_config)
+    let mut spi_driver = SpiDriver::new::<SPI2>(spi_pin, sclk, sdo, Some(sdi), &SpiDriverConfig::new())?;
+    spi::SpiBusDriver::new(spi_driver, &spi_config)//Crash here
 }
 
 #[derive(Deserialize, Debug)]
