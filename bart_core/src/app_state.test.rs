a4b31ed5a0c88b48e0f32faa66d6b80a817ec872
Tyler Casselman <tylerc230@gmail.com>
2024-05-14 15:50:48 -0700

    Test for rings filled based on json string

--- bart_core/src/app_state.test.rs
+++ bart_core/src/app_state.test.rs
@@ -1,164 +1,51 @@
 use crate::AppState;
+use smart_leds::RGB8;
+use smart_leds::colors;
+mod fixtures;
+
 #[test]
-fn test_add() {
+fn test_two_rings_lit() {
     let mut app_state = AppState::new();
-    app_state.received_http_response(JSON.to_string());
-    assert_eq!(app_state.minutes_until_next_trains[0], Some(4));
-    assert_eq!(app_state.minutes_until_next_trains[1], Some(15));
+    app_state.received_http_response(fixtures::json_with_etd("4", "15").to_string());
+    let led_buffer = app_state.get_current_led_buffer(0);
+
+    let mut expected_buffer: [RGB8; 44] = [colors::BLACK; 44];
+    fill_outer_ring::<15>(&mut expected_buffer, colors::YELLOW);
+    fill_inner_ring::<4>(&mut expected_buffer, colors::YELLOW);
+
+    assert_eq!(expected_buffer, led_buffer.rgb_buffer);
 }
 
-const JSON: &str = r##"{
-"?xml": {
-"@version": "1.0",
-"@encoding": "utf-8"
-},
-"root": {
-"@id": "1",
-"uri": {
-"#cdata-section": "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ROCK&json=y"
-},
-"date": "05/09/2024",
-"time": "04:24:03 PM PDT",
-"station": [
-{
-"name": "Rockridge",
-"abbr": "ROCK",
-"etd": [
-{
-"destination": "Antioch",
-"abbreviation": "ANTC",
-"limited": "0",
-"estimate": [
-{
-"minutes": "21",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "337",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "35",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "55",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-}
-]
-},
-{
-"destination": "Pittsburg/Bay Point",
-"abbreviation": "PITT",
-"limited": "0",
-"estimate": [
-{
-"minutes": "11",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "403",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "26",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "91",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "45",
-"platform": "1",
-"direction": "North",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-}
-]
-},
-{
-"destination": "SF Airport",
-"abbreviation": "SFIA",
-"limited": "0",
-"estimate": [
-{
-"minutes": "4",
-"platform": "2",
-"direction": "South",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "15",
-"platform": "2",
-"direction": "South",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-},
-{
-"minutes": "23",
-"platform": "2",
-"direction": "South",
-"length": "8",
-"color": "YELLOW",
-"hexcolor": "#ffff33",
-"bikeflag": "1",
-"delay": "0",
-"cancelflag": "0",
-"dynamicflag": "0"
-}
-]
-}
-]
-}
-],
-"message": ""
-}
-}"##;
+#[test]
+fn test_shortest_etd_too_long_for_inner_ring() {
+    let mut app_state = AppState::new();
+    app_state.received_http_response(fixtures::json_with_etd("17", "20").to_string());
+    let led_buffer = app_state.get_current_led_buffer(0);
 
+    let mut expected_buffer: [RGB8; 44] = [colors::BLACK; 44];
 
+    fill_outer_ring::<17>(&mut expected_buffer, colors::YELLOW);
 
+    assert_eq!(expected_buffer, led_buffer.rgb_buffer);
+}
+
+#[test]
+fn test_etd_is_LEAVING() {
+    let mut app_state = AppState::new();
+    app_state.received_http_response(fixtures::json_with_etd("LEAVING", "15").to_string());
+    let led_buffer = app_state.get_current_led_buffer(0);
+
+    let mut expected_buffer: [RGB8; 44] = [colors::BLACK; 44];
+    fill_outer_ring::<23>(&mut expected_buffer, colors::YELLOW);
+    fill_inner_ring::<15>(&mut expected_buffer, colors::YELLOW);
+
+    assert_eq!(expected_buffer, led_buffer.rgb_buffer);
+}
+
+fn fill_outer_ring<const N: usize>(buffer: &mut [RGB8; 44], color: RGB8) {
+    buffer[..N].clone_from_slice(&[color; N]);
+}
+
+fn fill_inner_ring<const N: usize>(buffer: &mut [RGB8; 44], color: RGB8) {
+    buffer[24..24+N].clone_from_slice(&[color; N]);
+}
---
+++ bart_core/src/fixtures.rs
@@ -0,0 +1,157 @@
+pub fn json_with_etd(first: &str, second:&str) -> String {
+    return format!(r##"{{
+"?xml": {{
+"@version": "1.0",
+"@encoding": "utf-8"
+}},
+"root": {{
+"@id": "1",
+"uri": {{
+"#cdata-section": "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ROCK&json=y"
+}},
+"date": "05/09/2024",
+"time": "04:24:03 PM PDT",
+"station": [
+{{
+"name": "Rockridge",
+"abbr": "ROCK",
+"etd": [
+{{
+"destination": "Antioch",
+"abbreviation": "ANTC",
+"limited": "0",
+"estimate": [
+{{
+"minutes": "21",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "337",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "35",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "55",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}}
+]
+}},
+{{
+"destination": "Pittsburg/Bay Point",
+"abbreviation": "PITT",
+"limited": "0",
+"estimate": [
+{{
+"minutes": "11",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "403",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "26",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "91",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "45",
+"platform": "1",
+"direction": "North",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}}
+]
+}},
+{{
+"destination": "SF Airport",
+"abbreviation": "SFIA",
+"limited": "0",
+"estimate": [
+{{
+"minutes": "{}",
+"platform": "2",
+"direction": "South",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "{}",
+"platform": "2",
+"direction": "South",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}},
+{{
+"minutes": "23",
+"platform": "2",
+"direction": "South",
+"length": "8",
+"color": "YELLOW",
+"hexcolor": "#ffff33",
+"bikeflag": "1",
+"delay": "0",
+"cancelflag": "0",
+"dynamicflag": "0"
+}}
+]
+}}
+]
+}}
+],
+"message": ""
+}}
+}}"##, first, second);
+}
+
+
+
