105729759e1e1e0d205545c5ac9d083431b64e28
Tyler Casselman <tylerc230@gmail.com>
2024-07-13 08:38:26 -0700

    Add motion sensor interrupt

--- bart-firmware/src/main.rs
+++ bart-firmware/src/main.rs
@@ -3,24 +3,11 @@ mod http;
 use bart_core::*;
 use anyhow::Result;
 use esp_idf_svc::{
-    eventloop::EspSystemEventLoop,
-    wifi::EspWifi,
-    hal::{
-        self,
-        task::thread::ThreadSpawnConfiguration,
-        task::queue::Queue,
-        peripheral::Peripheral,
-        prelude::Peripherals, 
-        spi::{
-            self, SPI2, SpiDriver, SpiDriverConfig
-        },
-        gpio,
-
-        cpu::Core,
-        units::FromValueType,
-        timer::config::Config as TimerConfig,
-        timer::TimerDriver
-    },
+    eventloop::EspSystemEventLoop, hal::{
+        self, cpu::Core, gpio::{self, Gpio4, InterruptType, PinDriver, Pull}, peripheral::Peripheral, prelude::Peripherals, spi::{
+            self, SpiDriver, SpiDriverConfig, SPI2
+        }, task::{self, queue::Queue, thread::ThreadSpawnConfiguration}, timer::{config::Config as TimerConfig, TimerDriver}, units::FromValueType
+    }, wifi::EspWifi
 };
 mod spi_driver;
 use spi_driver::Ws2812;
@@ -47,12 +34,19 @@ fn main() -> Result<()>{
     let peripherals = Peripherals::take().unwrap();
     let pins = peripherals.pins;
     let spi_pin = peripherals.spi2;
-    start_render_thread(pins, spi_pin, led_rx);
+    let motion_sensor_pin = pins.gpio4;
+    let sclk = Some(pins.gpio12);
+    let sdo = Some(pins.gpio11);
+    let sdi = Some(pins.gpio13);
+
+    let leds = LEDOutput::new(sclk, sdo, sdi, spi_pin, led_rx).unwrap();
+    start_render_thread(leds);
 
     let modem = peripherals.modem;
     let timer00 = peripherals.timer00;
     let timer01 = peripherals.timer01;
-    let mut shell = AppShell::new(led_tx, timer00, timer01)?;
+    let mut shell = AppShell::new(led_tx, timer00, timer01, motion_sensor_pin)?;
+    shell.start_motion_sensor()?;
     shell.start_render_timer()?;
 
     shell.connect_to_wifi(modem)?;
@@ -64,24 +58,32 @@ fn main() -> Result<()>{
 enum AppShellCommand {
     FetchSchedule,
     RenderLEDs,
+    MotionSensed,
     WifiConnected
 }
 struct AppShell<'a> {
     app_state: AppState,
     fetch_schedule_timer: TimerDriver<'a>,
     render_led_timer: TimerDriver<'a>,
+    motion_sensor: PinDriver<'a, Gpio4, hal::gpio::Input>,
     led_tx: mpsc::Sender<LEDIter>,
     command_queue: Queue<AppShellCommand>
 }
 
 impl AppShell<'_> 
 {
-    fn new<'a, T0: hal::timer::Timer, T1: hal::timer::Timer>(led_tx: mpsc::Sender<LEDIter>, timer00: impl Peripheral<P = T0> + 'a, timer01: impl Peripheral<P = T1> + 'a)-> Result<AppShell<'a>> {
+    fn new
+    <'a, T0: hal::timer::Timer, T1: hal::timer::Timer>
+    (led_tx: mpsc::Sender<LEDIter>, 
+        timer00: impl Peripheral<P = T0> + 'a, 
+        timer01: impl Peripheral<P = T1> + 'a, 
+        motion_sensor_pin: Gpio4)-> Result<AppShell<'a>> {
         let app_state = AppState::new();
         let command_queue = Queue::new(20);
         let fetch_schedule_timer = Self::create_command_timer(timer00, AppShellCommand::FetchSchedule, &command_queue, false)?;
         let render_led_timer = Self::create_command_timer(timer01, AppShellCommand::RenderLEDs, &command_queue, true)?;
-        let shell = AppShell { app_state, fetch_schedule_timer, render_led_timer, led_tx, command_queue };
+        let motion_sensor = Self::create_motion_sensor(motion_sensor_pin, &command_queue)?;
+        let shell = AppShell { app_state, fetch_schedule_timer, render_led_timer, motion_sensor, led_tx, command_queue };
         Ok(shell)
     }
 
@@ -110,6 +112,10 @@ impl AppShell<'_>
             AppShellCommand::WifiConnected => {
                 self.schedule_next_fetch(0)?;
             }
+            AppShellCommand::MotionSensed => {
+                log::info!("Motion Sensed");
+                self.start_motion_sensor();
+            }
         }
         Ok(())
     }
@@ -137,12 +143,17 @@ impl AppShell<'_>
         Ok(())
     }
 
+    fn start_motion_sensor(&mut self) -> Result<()> {
+        self.motion_sensor.enable_interrupt()?;
+        Ok(())
+    }
+
 
     fn create_command_timer<'d, T: hal::timer::Timer>(timer: impl Peripheral<P = T> + 'd, command: AppShellCommand, command_queue: &Queue<AppShellCommand>, repeat: bool) -> Result<TimerDriver<'d>> {
         let config = TimerConfig::new().auto_reload(repeat);
         let mut timer_driver = TimerDriver::new(timer, &config)?;
         unsafe {
-            let c = Self::new_queue(&command_queue);
+            let c = Self::new_queue(command_queue);
             timer_driver.subscribe(move || {
                 c.send_back(command, 100).unwrap();
             })?;
@@ -151,6 +162,21 @@ impl AppShell<'_>
         timer_driver.enable_interrupt()?;
         timer_driver.enable(true)?;
         Ok(timer_driver)
+    }
+
+    fn create_motion_sensor<'a>(pin: Gpio4, command_queue: &Queue<AppShellCommand>) -> Result<PinDriver<'a, Gpio4, hal::gpio::Input>> {
+        let mut motion_sensor = PinDriver::input(pin)?;
+        motion_sensor.set_pull(Pull::Down)?;
+        motion_sensor.set_interrupt_type(InterruptType::PosEdge)?;
+        unsafe {
+            let c = Self::new_queue(command_queue);
+            motion_sensor.subscribe(move || {
+                c.send_front(AppShellCommand::MotionSensed, 100).unwrap();
+                //log::info!("MOTION SENSED");
+            })?;
+        }
+        Ok(motion_sensor)
+
     }
 
     fn connect_to_wifi(&mut self, modem: impl hal::peripheral::Peripheral<P = hal::modem::Modem> + 'static + std::marker::Send) -> Result<()> {
@@ -201,7 +227,7 @@ impl AppShell<'_>
 }
 
 
-fn start_render_thread(pins: gpio::Pins, spi_pin: spi::SPI2, led_rx: mpsc::Receiver<LEDIter>) {
+fn start_render_thread(mut leds: LEDOutput) {
     let config = ThreadSpawnConfiguration {
         name: Some(b"RenderThread\0"),
         stack_size: 4096,
@@ -218,34 +244,39 @@ fn start_render_thread(pins: gpio::Pins, spi_pin: spi::SPI2, led_rx: mpsc::Recei
         .spawn(move || {
 
             log::info!("Render loop core: {:?}", esp_idf_svc::hal::cpu::core());
-            let mut leds = LEDOutput::<'_>::new(pins, spi_pin, led_rx).unwrap();
             leds.render_loop();
         })
         .unwrap();
 }
 
-struct LEDOutput<'a> {
-    leds: LEDs<'a>,
+struct LEDOutput {
+    sclk: Option<gpio::Gpio12>,
+    sdo: Option<gpio::Gpio11>,
+    sdi: Option<gpio::Gpio13>,
+    spi_device: Option<spi::SPI2>,
     led_rx: mpsc::Receiver<LEDIter>
 }
 
-impl<'a> LEDOutput<'a> {
-    fn new(pins: gpio::Pins, spi_pin: spi::SPI2, led_rx: mpsc::Receiver<LEDIter>) -> Result<LEDOutput<'a>> {
-        let sclk = pins.gpio12;
-        let sdo = pins.gpio11;
-        let sdi = pins.gpio13;
-        let spi_config = spi::SpiConfig::new().baudrate(6410.kHz().into());
-        let spi_driver = SpiDriver::new::<SPI2>(spi_pin, sclk, sdo, Some(sdi), &SpiDriverConfig::new())?;
-        let spi_bus = spi::SpiBusDriver::new(spi_driver, &spi_config)?;
-        let leds = Ws2812::new(spi_bus);
-        Ok(LEDOutput { leds, led_rx })
+    
+impl LEDOutput {
+    fn new(sclk: Option<gpio::Gpio12>, sdo: Option<gpio::Gpio11>, sdi: Option<gpio::Gpio13>, spi_device: spi::SPI2, led_rx: mpsc::Receiver<LEDIter>) -> Result<LEDOutput> {
+        Ok(LEDOutput { sclk, sdo, sdi, spi_device: Some(spi_device), led_rx })
     }
 
-    fn render_loop(&mut self) {
+    fn create_leds<'a>(& mut self) -> Result<LEDs<'a>> {
+        let spi_config = spi::SpiConfig::new().baudrate(6410.kHz().into());
+        let spi_driver = SpiDriver::new::<SPI2>(self.spi_device.take().unwrap(), self.sclk.take().unwrap(), self.sdo.take().unwrap(), self.sdi.take(), &SpiDriverConfig::new())?;
+        let spi_bus = spi::SpiBusDriver::new(spi_driver, &spi_config)?;
+        Ok(Ws2812::new(spi_bus))
+    }
+
+    fn render_loop(&mut self) -> Result<()> {
+        let mut leds = self.create_leds()?;
         for led_buffer in &self.led_rx {
             log::debug!("Rendering leds");
-            self.leds.write(led_buffer).unwrap();
+            leds.write(led_buffer).unwrap();
         }
+        Ok(())
     }
 }
 
